178d8623180632c329753aeb5a9cf4f4775eaae0
Ilse Macías <ilse.macias@gmail.com>
2018-05-04 15:57:27 -0700

    1. Verify item in Mobile List page can be sorted by 'Name'.

--- PageObjectModel/Guru99POM/TestCases/TC01.cs
+++ PageObjectModel/Guru99POM/TestCases/TC01.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using NUnit.Framework;
-using OpenQA.Selenium;
-using System.Threading;
+﻿using NUnit.Framework;
 
 namespace Guru99POM
 {
@@ -15,33 +12,6 @@ namespace Guru99POM
 
             MobilePOM mobile = homePage.ClickOnMobileLink();                     
             mobile.SelectSort();
-        }
-
-        [Test, Description("2. Verify that cost of product in list page and details page are equal.")]
-        public void VerifyCostOfProducts()
-        {
-            //  MobilePageSortByName();
-            HomePagePOM homePage = new HomePagePOM(Properties.driver);
-
-            MobilePOM mobile = homePage.ClickOnMobileLink();
-            mobile.CostSonyXperiaMobile();
-
-            MobileDetailsPOM mobileDetails = mobile.SeeDetailsOfMobileProduct();
-            mobileDetails.SonyXperiaDetails();
-        }
-
-        [Test, Description("3. Verify that you can't add more product in cart than the product available in store.")]
-        public void ErrorVerification()
-        {
-            HomePagePOM homePage = new HomePagePOM(driver);
-
-            MobilePOM mobile = homePage.ClickOnMobileLink();
-
-            AddToCartPOM cart = mobile.AddToCartMobile();
-            cart.AddQuantityAndUpdate();
-            cart.VerifyErrorMessage();
-
-            cart.ClickOnEmptyCart();
         }
     }
 }
