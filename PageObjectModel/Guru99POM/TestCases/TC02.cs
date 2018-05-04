86993063f0ce48cc7dcf0900622f9a3e682235df
Ilse Macías <ilse.macias@gmail.com>
2018-05-04 15:59:27 -0700

    2. Verify that cost of product in list page and details page are equal.

---
+++ PageObjectModel/Guru99POM/TestCases/TC02.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace Guru99POM.TestCases
+{
+    class TC02 : TestBase
+    {
+        [Test, Description("2. Verify that cost of product in list page and details page are equal.")]
+        public void VerifyCostOfProducts()
+        {
+            //  MobilePageSortByName();
+            HomePagePOM homePage = new HomePagePOM(Properties.driver);
+
+            MobilePOM mobile = homePage.ClickOnMobileLink();
+            mobile.CostSonyXperiaMobile();
+
+            MobileDetailsPOM mobileDetails = mobile.SeeDetailsOfMobileProduct();
+            mobileDetails.SonyXperiaDetails();
+        }
+    }
+}
